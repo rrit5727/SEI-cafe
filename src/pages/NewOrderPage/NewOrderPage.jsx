0f7a6614d537dddd2faede54ddb2bc12d685df3b
rrit5727 <rrit5727@gmail.com>
2024-03-20 16:57:43 +1100

    first commit

---
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+import './NewOrderPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+export default function NewOrderPage({ user, setUser }) {
+  const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState('');
+  const categoriesRef = useRef([]);
+
+  // The empty dependency array causes the effect
+  // to run ONLY after the FIRST render
+  useEffect(function() {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      categoriesRef.current = [...new Set(items.map(item => item.category.name))];
+      setMenuItems(items);
+      setActiveCat(categoriesRef.current[0]);
+    }
+    getItems();
+  }, []);
+
+  return (
+    <main className="NewOrderPage">
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <MenuList
+        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+      />
+      <OrderDetail />
+    </main>
+  );
+}
